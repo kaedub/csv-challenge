0d425407e0b825c2c2b1877d8d48d5f28fff75ae
Kevin Welch <kaedub16@gmail.com>
2019-03-17 04:00:24 -0700

    Moved validation functions to file from class methods in app.py

---
+++ validation.py
@@ -0,0 +1,33 @@
+from config import ZIP_LENGTH, PHONE_LENGTH
+
+def _is_name(name):
+    return name.isalpha()
+
+def _is_phone(phone):
+    """Returns true if 'phone' is a valid phone number"""
+    return _is_phone_spaced(phone) or _is_phone_hyphenated(phone)
+
+def _is_zip(zip):
+    """Returns true if 'zip' is a valid zip code"""
+    return zip.isdigit() and len(zip) == 5
+
+def _is_phone_hyphenated(phone):
+    """Returns true if 'phone' is a valid phone number with parentheses and hyphens"""
+    stripped = ''.join(phone.split('-'))
+    if len(stripped) < PHONE_LENGTH+2:
+        return False
+    elif stripped[0] != '(' and stripped[4] != ')':
+        return False
+    raw_phone = stripped[1:4] + stripped[5:]
+    return raw_phone.isdigit() and len(raw_phone) == PHONE_LENGTH
+
+def _is_phone_spaced(phone):
+    """Returns true if 'phone' is a valid phone number with spaces instead of dashes and parentheses"""
+    raw_phone = ''.join(phone.split(' '))
+    return len(raw_phone) == 10 and raw_phone.isdigit()
+
+validation = {
+    "phone": _is_phone,
+    "zip": _is_zip,
+    "name": _is_name,
+}

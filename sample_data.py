da7b3c8a8f1ec223bf74d0bada3396d6da28526c
Kevin Welch <kaedub16@gmail.com>
2019-03-18 16:16:13 -0700

    Added sample data to handle more edge cases

--- sample_data.py
+++ sample_data.py
@@ -1,11 +1,23 @@
 test_case_data = {
-    "valid_phones": [
-        '703 742 0996',
-        '646 111 0101',
-        '703 955 0373',
+    "valid_lines": [
+        "Smith, John, (623)-668-9293, yellow, 87360",
+        "James Murphy, yellow, 83880, 018 154 6474"
+        "Booker T., Washington, 87360, 373 781 7380, yellow",
+    ],
+    "valid_formats": [
+        "lastname,firstname,phone_hyphenated,color,zip",
+        "fullname,color,zip,phone_spaced",
+        "firstname,lastname,zip,phone_spaced,color"
+    ],
+    "valid_phones_hyphenated": [
         '(703)-742-0996',
         '(646)-111-0101',
         '(703)-955-0373',
+    ],
+    "valid_phones_spaced": [
+        '703 742 0996',
+        '646 111 0101',
+        '703 955 0373',
     ],
     "invalid_phones": [
         '(703) 742 0996',
@@ -36,7 +48,7 @@ test_case_data = {
     ],
     "invalid_names": [
         'this-is-not-a-valid-name',
-        'this may be a valid name',
+        'this.is.not.a.valid.name?',
         'invalid333',
         'n4m3',
     ]
